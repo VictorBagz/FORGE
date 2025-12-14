d2fec9a2c557adaa3f9f11a5b59a1dffc0ff2f36
VictorBagz <victorbaguma@example.com>
2025-12-14 19:43:52 +0300

    modified

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,7 +7,7 @@ const Hero: React.FC = () => {
       {/* Background Image with Overlay */}
       <div className="absolute inset-0 z-0">
         <img
-          src="https://images.unsplash.com/photo-1534438327276-14e5300c3a48?q=80&w=2070&auto=format&fit=crop"
+          src="./forge1.jpeg"
           alt="Gym background"
           className="w-full h-full object-cover"
         />
